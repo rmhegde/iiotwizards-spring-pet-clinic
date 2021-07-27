0e737bb54c3fa485c3f0de6f7dd577f7e2987d03
rmhegde <rmhegde72@gmail.com>
2021-07-27 23:25:44 +0530

    Initial Draft

---
+++ src/main/java/iiotwizards/petclinic/PetClinicApplication.java
@@ -0,0 +1,15 @@
+package iiotwizards.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
